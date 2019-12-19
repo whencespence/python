1718489832bbeca5e67916ce3557f144294509df
Christina Spencer <hello@christinaspencer.ca>
2019-12-19 18:56:03 -0500

    Updated

---
+++ project/campaign_contributions_by_nominee.py
@@ -0,0 +1,42 @@
+#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+# ms-python.python added
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'python-programming/project'))
+	print(os.getcwd())
+except:
+	pass
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as ply
+import seaborn as sns
+
+
+#%%
+pd.set_option('display.max_columns', None)
+
+
+#%%
+def print_full(x):
+    pd.set_option('display.max_rows', len(x))
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 2000)
+    pd.set_option('display.float_format', '{:20,.2f}'.format)
+    pd.set_option('display.max_colwidth', -1)
+    print(x)
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.width')
+    pd.reset_option('display.float_format')
+    pd.reset_option('display.max_colwidth')
+
+
+#%%
+data
+
+
+#%%
+
+
+
